71ee4456e086a5654f60a4c595a9ff322240cbc7
DMartin <davidmartin8127@gmail.com>
2022-12-25 21:30:09 -0800

    finished renderHTML function and added cards for each role

--- index.js
+++ index.js
@@ -55,10 +55,40 @@ function newEmployee() {
                 })
                 break;
             case 'Intern':
-                //ask about school
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: 'What school does the intern attend?'
+                    }
+                ]).then(({ school }) => {
+                    employees.push(new Intern(
+                        name,
+                        id,
+                        email,
+                        school
+                    ))
+
+                    another()
+                })
                 break;
             case 'Engineer':
-                //ask about github
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'Github',
+                        message: "What is the engineer's Github username?"
+                    }
+                ]).then(({ Github }) => {
+                    employees.push(new Engineer(
+                        name,
+                        id,
+                        email,
+                        Github
+                    ))
+
+                    another()
+                })
                 break;
             default:
             //Woops
@@ -79,22 +109,101 @@ function another() {
     })
 }
 
+//function to create HTML file using collected user inputs
 function renderHTMLFile() {
     fs.writeFileSync('./index.html', /*html*/ `
-        <ul>
-            ${employees.map(employee => /*html*/`
-            <li>
-                <div>
-                    <h1>${employee.getName()}</h1>
-                    <a href="mailto:${employee.getEmail()}">${employee.getEmail()}<a>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <title>Team Profile</title>
+        </head>
+        <body>
+            <div class="jumbotron text-center">
+                <h1>My Team Profile</h1>
+            </div>
+            <div class= "container">
+                <div class= "row col-12">
+                ${employees.map(employee => /*html*/ {
+        switch (employee.getRole()) {
+            case "Manager":
+                return managerCard(employee);
+            case "Engineer":
+                return engineerCard(employee);
+            case "Intern":
+                return internCard(employee);
+            }
+        }
+    )}
                 </div>
-            </li>
-            `)}
-        </ul>
+            </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>            
+        </body>
     `)
 }
 
+
+
 //for github link down below:
 //<a href="https://github.com/${employee.getGithub()}">${employee.getGithub()}<a>
+
+
+//Cards that return HTML content depending on what type of employee they are
+function managerCard(manager) {
+    return `
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">${manager.name} 💼</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+            <li class="list-group-item">employee ID: ${manager.id}</li>
+            <li class="list-group-item">office number: ${manager.officeNumber}</li>
+            <li class="list-group-item">email: ${manager.email}</li>
+            </ul>
+        </div>
+      </div>
+    `;
+}
+
+function engineerCard(engineer) {
+    return `
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">${engineer.name} ⚙ </h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+            <li class="list-group-item">employee ID: ${engineer.id}</li>
+            <li class="list-group-item">email: ${engineer.email}</li>
+            <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}<a></li>
+            </ul>
+        </div>
+      </div>
+    `;
+}
+
+function internCard(intern) {
+    return `
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">${intern.name} 🎓</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+            <li class="list-group-item">employee ID: ${intern.id}</li>
+            <li class="list-group-item">email: ${intern.email}</li>
+            <li class="list-group-item">School: ${intern.school}</li>
+            </ul>
+        </div>
+      </div>
+    `;
+}
+
 
 newEmployee()
